5362f0a1d4f5790974b4ecd72ea394be5ffd90a2
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2025-03-01 15:39:28 +0100

    Main dashboard grid layout

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="h-screen grid grid-cols-[14rem_1fr] grid-rows-[6rem_1fr] grid-">
+    <div class="bg-red-100 row-span-2">
+      sidebar
+    </div>
+    <div class="bg-blue-100">
+      topbar
+    </div>
+    <div class="bg-green-100">
+      main
+    </div>
+  </main>
+  <div class="bg-purple-100 col-span-2 min-h-80">
+    footer
+  </div>
+</template>
