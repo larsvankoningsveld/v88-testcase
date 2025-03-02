dd5a60a7c3ed9d3f0cb0966fd7b614ecc53caf3d
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2025-03-02 12:35:51 +0100

    Add topbar and links

---
+++ components/Topbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="bg-white border-l-3 border-grey-50 flex h-full items-center justify-between">
+    <ul class="flex gap-16 px-8">
+      <li><a href="/">Logo</a></li>
+      <li><a href="#">Hoe werkt het?</a></li>
+      <li><a href="#">Over ons</a></li>
+    </ul>
+    <ul class="flex gap-16 px-8 mr-32">
+      <li><a href="#">Blog</a></li>
+      <li><a href="#">Kenniscentrum</a></li>
+      <li><a href="#">Help</a></li>
+    </ul>
+  </nav>
+</template>
