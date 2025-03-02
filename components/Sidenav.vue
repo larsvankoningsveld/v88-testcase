1d6ca5d7aa77c65728ee5f6881bff4520bfbf8ca
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2025-03-02 21:39:07 +0100

    Add icons into navbar

--- components/Sidenav.vue
+++ components/Sidenav.vue
@@ -1,15 +1,22 @@
 <script setup>
 import { ref } from 'vue'
+import dashboard from 'assets/svg/dashboard.svg'
+import projects from 'assets/svg/projects.svg'
+import messages from 'assets/svg/messages.svg'
+import ownProjects from 'assets/svg/own-projects.svg'
+import notifications from 'assets/svg/notifications.svg'
+import todos from 'assets/svg/todos.svg'
+import profile from 'assets/svg/profile.svg'
+
 const collapsed = ref(false)
 
 const navItems = ref([
-  { name: 'Dashboard', icon: '🐶', link: '#' },
-  { name: 'Projecten', icon: '🐱', link: '#' },
-  { name: 'Berichten', icon: '🐭', link: '#' },
-  { name: 'Eigen Projecten', icon: '🐹', link: '/' },
-  { name: 'Notificaties', icon: '🐰', link: '#' },
-  { name: 'To-do\'s', icon: '🦊', link: '#' },
-  { name: 'Profiel', icon: '🐻', link: '#' }
+  { name: 'Dashboard', icon: dashboard, link: '#' }, { name: 'Projecten', icon: projects, link: '#' },
+  { name: 'Berichten', icon: messages, link: '#' },
+  { name: 'Eigen Projecten', icon: ownProjects, link: '/' },
+  { name: 'Notificaties', icon: notifications, link: '#' },
+  { name: 'To-do\'s', icon: todos, link: '#' },
+  { name: 'Profiel', icon: profile, link: '#' }
 ])
 </script>
 
@@ -17,18 +24,18 @@ const navItems = ref([
   <div class="flex flex-col p-4 gap-16 bg-white h-full">
     <button @click="collapsed = !collapsed" class="self-end">
       <div v-if="collapsed">
-        -> </div>
+        <img src="assets/svg/unfold.svg" alt="">
+      </div>
       <div v-else>
-        <- </div>
+        <img src="assets/svg/collapse.svg" alt="">
+      </div>
     </button>
     <nav>
       <ul class="flex flex-col gap-8">
         <li v-for="item in navItems">
           <a :href="item.link">
             <div class="flex gap-4 px-2 py-1 hover:bg-blue-50 active:bg-blue-50 rounded-lg">
-              <div>
-                {{ item.icon }}
-              </div>
+              <img :src="item.icon" alt="#" class="h-4 w-4">
               <div :class="{ hidden: collapsed }">
                 {{ item.name }}
               </div>
