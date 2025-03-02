204b1c6d1d7564061d006a7a87167e9c2cb195f9
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2025-03-02 17:50:20 +0100

    Add margin to title

--- pages/index.vue
+++ pages/index.vue
@@ -4,14 +4,16 @@ import { mockProjects } from '~/assets/data/ownProjects';
 </script>
 <template>
   <div>
-    <div class="flex justify-between">
+    <div class="flex justify-between mb-16">
       <h1 class="text-4xl">Eigen projecten</h1>
       <button class="rounded-3xl px-4 py-2 bg-blue-900 text-white">
         + Nieuw project maken
       </button>
     </div>
-    <div v-for="project in mockProjects">
-      <OwnProject :project="project" />
+    <div class="grid grid-cols-2 gap-8">
+      <div v-for="project in mockProjects">
+        <OwnProject :project="project" />
+      </div>
     </div>
   </div>
 </template>
