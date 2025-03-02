862bc17af68a90ef3179d3acf56a1fd26b08d297
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2025-03-02 17:49:46 +0100

    Add temporary coloured border

--- components/OwnProject.vue
+++ components/OwnProject.vue
@@ -2,30 +2,32 @@
 const props = defineProps(['project'])
 </script>
 <template>
-  <div class="bg-white rounded-xl p-4 flex flex-col gap-4">
+  <div class="bg-white rounded-xl p-8 flex flex-col gap-4">
     <div class="flex justify-between">
-      <div id="title-section" class="grow">
-        <div class="font-bold text-xl">
-          {{ project.title }}
-        </div>
-        <div class="text-lg">
-          {{ project.system }}
-        </div>
-        <div class="flex justify-between">
-          <div class="flex flex-col">
-            <div class="font-bold">
-              {{ project.sector }}
-            </div>
-            <div class="text-sm">
-              {{ project.sector }}
-            </div>
+      <div id="title-section" class="grow flex gap-2">
+        <div class="border-l-4 border-orange-300 px-2">
+          <div class="font-bold text-xl">
+            {{ project.title }}
           </div>
-          <div class="flex flex-col">
-            <div class="font-bold">
-              {{ project.sector }}
+          <div class="text-lg">
+            {{ project.system }}
+          </div>
+          <div class="flex justify-between">
+            <div class="flex flex-col">
+              <div class="font-bold">
+                {{ project.sector }}
+              </div>
+              <div class="text-sm">
+                {{ project.sector }}
+              </div>
             </div>
-            <div class="text-sm">
-              {{ project.sector }}
+            <div class="flex flex-col">
+              <div class="font-bold">
+                {{ project.sector }}
+              </div>
+              <div class="text-sm">
+                {{ project.sector }}
+              </div>
             </div>
           </div>
         </div>
