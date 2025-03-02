8eb42b98ad8201e29b64b9b7dfd18708dd880c9b
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2025-03-02 12:38:25 +0100

    Add styling to logo and center items

--- components/Topbar.vue
+++ components/Topbar.vue
@@ -1,11 +1,14 @@
 <template>
   <nav class="bg-white border-l-3 border-grey-50 flex h-full items-center justify-between">
-    <ul class="flex gap-16 px-8">
-      <li><a href="/">Logo</a></li>
+    <ul class="flex gap-16 px-8 items-center">
+      <li><a href="/">
+          <div class="px-6 py-2 bg-green-500 rounded text-white">Logo</div>
+        </a>
+      </li>
       <li><a href="#">Hoe werkt het?</a></li>
       <li><a href="#">Over ons</a></li>
     </ul>
-    <ul class="flex gap-16 px-8 mr-32">
+    <ul class="flex gap-16 px-8 mr-32 items-center">
       <li><a href="#">Blog</a></li>
       <li><a href="#">Kenniscentrum</a></li>
       <li><a href="#">Help</a></li>
