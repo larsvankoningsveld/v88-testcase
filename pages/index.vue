43b297df3fb70e5f9b76d92c68abd25a902ce1bb
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2025-03-03 13:15:28 +0100

    Add list for systems

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,45 @@
 <script setup>
-import { mockProjects } from '~/assets/data/ownProjects';
-import plus from "assets/svg/plus.svg"
+import { mockProjects } from "~/assets/data/ownProjects";
+import plus from "assets/svg/plus.svg";
+
+const systemList = [
+  {
+    label: "Systeem 1",
+    checked: false,
+  },
+  {
+    label: "Systeem 2",
+    checked: true,
+  },
+  {
+    label: "Systeem 3",
+    checked: false,
+  },
+  {
+    label: "Systeem 4",
+    checked: false,
+  },
+  {
+    label: "Systeem 5",
+    checked: false,
+  },
+];
 </script>
 
 <template>
   <div>
     <div class="flex justify-between mb-16">
       <h1 class="text-5xl font-medium">Eigen projecten</h1>
-      <a href="#" class="hover:cursor-pointer rounded-3xl items-center flex gap-4 px-8 py-2 bg-blue-900 text-white">
-        <img class="w-[16px]" :src="plus" alt="+">
-        <span class="text-lg font-light">
-          Nieuw project maken
-        </span>
+      <a href="#" class="btn-primary items-center flex gap-4">
+        <img class="w-[16px]" :src="plus" alt="+" />
+        <span class="text-lg font-light"> Nieuw project maken </span>
       </a>
     </div>
     <div class="flex gap-4 mb-8">
-      <Filter id="filter-1" />
-      <Filter id="filter-2" />
-      <Filter id="filter-3" />
+      <Filter id="filter-1" label="Systeem" :list="systemList" />
+      <Filter id="filter-2" label="Sector" />
+      <Filter id="filter-3" label="Urgentie" />
+      <Filter id="filter-4" label="Status" />
     </div>
     <div class="grid grid-cols-2 gap-4">
       <div v-for="project in mockProjects">
