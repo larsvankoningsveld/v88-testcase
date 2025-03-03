402b50eefa1b1ae232c6d5c57312932043605d8c
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2025-03-03 13:15:43 +0100

    Add buttons and loop over list

--- components/Filter.vue
+++ components/Filter.vue
@@ -1,12 +1,38 @@
 <script setup>
-const props = defineProps(['id'])
-
+const props = defineProps(["id", "label", "list"]);
+import chevron from "assets/svg/chevron.svg";
 </script>
 
 <template>
   <div class="relative">
-    <button class="px-6 py-1 rounded bg-white shadow text-grey-500" :popovertarget="id">Greet me</button>
-    <div :id="id" class="inset-[unset] bg-white p-16 shadow rounded-xl relative mt-2" popover="auto">Hi there!
+    <button
+      class="flex gap-4 hover:cursor-pointer items-center hover:shadow-lg active:shadow-md px-4 py-1 rounded-lg bg-white shadow text-grey-400"
+      :popovertarget="id"
+    >
+      <div>
+        {{ label }}
+      </div>
+      <img :src="chevron" alt="#" class="w-[24px]" />
+    </button>
+    <div
+      :id="id"
+      class="inset-[unset] bg-white p-8 shadow rounded-xl relative mt-2"
+      popover="auto"
+    >
+      <div v-for="item in list">
+        <ul>
+          <li class="flex gap-2 items-center">
+            <input type="checkbox" :checked="item.checked" />
+            <span>
+              {{ item.label }}
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div class="flex gap-2">
+        <button class="btn-secondary">Annuleer</button>
+        <button class="btn-primary">Bevestigen</button>
+      </div>
     </div>
   </div>
 </template>
