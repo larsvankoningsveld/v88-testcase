de89312c7cde207cbd797bd52e6dd62ff2244f15
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2025-03-03 10:02:18 +0100

    Add plus icon to new project button

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,21 @@
 <script setup>
 import { mockProjects } from '~/assets/data/ownProjects';
-
+import plus from "assets/svg/plus.svg"
+function hi() {
+  alert("hi")
+}
 </script>
+
 <template>
   <div>
     <div class="flex justify-between mb-16">
       <h1 class="text-5xl font-medium">Eigen projecten</h1>
-      <button class="rounded-3xl px-8 py-2 bg-blue-900 text-white">
-        + Nieuw project maken
-      </button>
+      <a href="#" class="hover:cursor-pointer rounded-3xl items-center flex gap-2 px-8 py-2 bg-blue-900 text-white">
+        <img class="w-[16px]" :src="plus" alt="+">
+        <span>
+          Nieuw project maken
+        </span>
+      </a>
     </div>
     <div class="grid grid-cols-2 gap-8">
       <div v-for="project in mockProjects">
