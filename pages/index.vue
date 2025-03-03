2ef46818b80708730e1c1c3037132da44d101dbb
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2025-03-03 10:28:48 +0100

    slightly change font size on new project button

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,15 @@
 <script setup>
 import { mockProjects } from '~/assets/data/ownProjects';
 import plus from "assets/svg/plus.svg"
-function hi() {
-  alert("hi")
-}
 </script>
 
 <template>
   <div>
     <div class="flex justify-between mb-16">
       <h1 class="text-5xl font-medium">Eigen projecten</h1>
-      <a href="#" class="hover:cursor-pointer rounded-3xl items-center flex gap-2 px-8 py-2 bg-blue-900 text-white">
+      <a href="#" class="hover:cursor-pointer rounded-3xl items-center flex gap-4 px-8 py-2 bg-blue-900 text-white">
         <img class="w-[16px]" :src="plus" alt="+">
-        <span>
+        <span class="text-lg font-light">
           Nieuw project maken
         </span>
       </a>
