680cb685f8475f938a033301a159fc71d6133f02
Lars van Koningsveld <larsvankoningsveld@gmail.com>
2025-03-03 10:23:30 +0100

    Finish card styling

--- components/OwnProject.vue
+++ components/OwnProject.vue
@@ -2,28 +2,28 @@
 const props = defineProps(['project'])
 </script>
 <template>
-  <div class="bg-white rounded-xl shadow p-8 flex flex-col gap-4">
+  <div class="bg-white rounded-xl shadow p-8 flex flex-col gap-6">
     <div class="flex justify-between">
       <div id="title-section" :class="project.status === 0 ? 'border-orange-300' : 'border-blue-200'"
-        class=" border-l-4  px-2 grow flex flex-col gap-2">
-        <div class="font-bold text-xl">
+        class=" border-l-4  px-3 grow flex flex-col gap-2">
+        <div class="font-bold text-2xl">
           {{ project.title }}
         </div>
-        <div class="text-lg">
+        <div class="text-xl">
           {{ project.system }}
         </div>
-        <div class="flex gap-24">
+        <div class="flex gap-38">
           <div class="flex flex-col">
-            <div class="text-sm">
-              {{ project.sector }}
+            <div class="text-sm text-grey-300">
+              Sector
             </div>
             <div class="font-bold">
               {{ project.sector }}
             </div>
           </div>
           <div class="flex flex-col">
-            <div class="text-sm">
-              {{ project.sector }}
+            <div class="text-sm text-grey-300">
+              Sector
             </div>
             <div class="font-bold">
               {{ project.sector }}
@@ -42,7 +42,7 @@ const props = defineProps(['project'])
     </div>
     <div id="desc">{{ project.desc }}</div>
     <div>
-      <a href="#" class="rounded-3xl bg-green-500 px-6 py-2 text-white">
+      <a href="#" class="rounded-3xl text-lg font-weight-extralight bg-green-500 px-6 py-2 text-white">
         Meer informatie
       </a>
     </div>
